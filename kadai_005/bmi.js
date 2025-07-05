42ed36288f5e95ae59abe52b446babccbf3fd962
moco56 <moesasaki.1056@gmail.com>
2025-07-06 02:50:59 +0900

    Delete kadai_005 directory

--- kadai_005/bmi.js
+++
@@ -1,7 +0,0 @@
-let userWeight;
-let userHeight;
-
-userWeight = 68;
-userHeight = 1.7;
-
-console.log (userWeight/(userHeight*userHeight));
--- kadai_005/kadai_005/bmi.js
+++
@@ -1,9 +0,0 @@
-let userWeight;
-let userHeight;
-
-userWeight = 68;
-userHeight = 1.7;
-
-let userBmi = userWeight / (userHeight * userHeight);
-
-console.log(userBmi);
